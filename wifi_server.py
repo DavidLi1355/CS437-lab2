48930003f72bed48d8f9e0723908caee3acbda64
David Li <lid1355218215@outlook.com>
2023-03-02 18:01:37 -0600

    implement movement control on server side

--- wifi_server.py
+++ wifi_server.py
@@ -1,4 +1,14 @@
 import socket
+import json
+
+# picar
+import picar_4wd as fc
+
+speed = 10
+turn_speed = 5
+curr_speed = 0
+
+distance_traveled = 0
 
 HOST = "172.16.252.49" # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
@@ -14,19 +24,32 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
             
             if data == "0":
-                # car stop
+                # stop
+                fc.stop() 
+                curr_speed = 0
             elif data == "87":
                 # w
+                fc.forward(speed)
+                curr_speed = speed
             elif data == "83":
                 # s
+                fc.backward(speed)
+                curr_speed = -speed
             elif data == "65":
                 # a
+                fc.turn_left(turn_speed)
+                curr_speed = 0
             elif data == "68":
                 # d
+                fc.turn_right(turn_speed)
+                curr_speed = 0
 
-            if data != b"":
-                print(data)     
-                client.sendall(data) # Echo back to client
+
+            data = {
+                "curr_speed" : curr_speed,
+            }
+            print(json.dumps(data))
+            client.sendall(json.dumps(data)) # Echo back to client
     except: 
         print("Closing socket")
         client.close()
